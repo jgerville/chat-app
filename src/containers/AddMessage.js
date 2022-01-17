98356601e361fb494bd27d2377cb0b40a492ddf1
Julian Erville <jerville@gmail.com>
2022-01-17 13:48:56 -0500

    Add basic skeleton

---
+++ src/containers/AddMessage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AddMessage = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+AddMessage.propTypes = {
+
+}
+
+export default AddMessage
