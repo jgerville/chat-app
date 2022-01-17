01d56050a086edbf51982c99d6002b7e21d4c8c5
Julian Erville <jerville@gmail.com>
2022-01-17 14:50:13 -0500

    Add AddMessage and Message components

--- src/containers/AddMessage.js
+++ src/containers/AddMessage.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const AddMessage = props => {
+const AddMessage = ({ dispatch }) => {
+  let input;
+
+  const handleInput = (e) => {
+    if (e.key === 'Enter') {
+      dispatch(input.value, 'Me')
+      input.value = ''
+    }
+  }
+  
   return (
-    <div>
-      
-    </div>
+    <section id='new-message'>
+      <input 
+        type='text' 
+        onKeyPress={handleInput}
+        ref={(node) => {input = node}}
+      />
+    </section>
   )
 }
 
 AddMessage.propTypes = {
-
+  dispatch: PropTypes.func.isRequired,
 }
 
-export default AddMessage
+export default AddMessage;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import rootReducer from './reducers/root';
 import App from './App';
 
-const store = createStore();
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
